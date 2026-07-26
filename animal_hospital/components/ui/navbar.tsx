82e25368097f3169d8b1ac6d96f6a4bfe58a14d1
Vultus Limunaria <danobrovold@gmail.com>
2026-07-26 02:08:50 +0200

    Add Menu Popover on Mobile

--- animal_hospital/components/ui/navbar.tsx
+++ animal_hospital/components/ui/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Typograpy from "./typography";
 import { Button } from "@base-ui/react";
 import Image from "next/image";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 export default function Navbar(){
     return (
@@ -15,9 +16,25 @@ export default function Navbar(){
                 <Link href={"/"}>Contact Us</Link>
             </div>
 
-            <Button className={"flex sm:hidden"}>
-                <Image src={"/menu.png"} height={30} width={30} alt="Menu"></Image>
-            </Button>
+            <Popover>
+                <PopoverTrigger render={
+                    <Button className={"flex sm:hidden"}>
+                        <Image src={"/menu.png"} height={30} width={30} alt="Menu"></Image>
+                    </Button>}>
+                </PopoverTrigger>
+
+                <PopoverContent className={"bg-white text-center"}>
+                    <Typograpy variant="h2" isUnderline isBold>Our Links</Typograpy>
+                    <div className="flex flex-col gap-5 text-lg">
+                        <Link href={"/"}>About the Hospital</Link>
+                        <Link href={"/"}>Meet the Staff</Link>
+                        <Link href={"/"}>Our Services</Link>
+                        <Link href={"/"}>Contact Us</Link>
+                    </div>
+                </PopoverContent>
+
+            </Popover>
+            
         </nav>
     )   
 }
